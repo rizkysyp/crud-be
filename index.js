8ae73f6273a80f7dd7bfb7c651c8ad3b74aed40b
exploding60 <rizkysyahputra14045@gmail.com>
2023-02-08 17:33:25 +0700

    Cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(morgan("dev"));
 
 const corsOptions = {
-  origin: "https://crud.rizkyproject.my.id/",
+  origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
